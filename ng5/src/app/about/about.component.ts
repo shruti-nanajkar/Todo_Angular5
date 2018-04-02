511c22caa11d27ad838c9f1b643ac974702a82ba
Shruti Nanajkar <nanajkar.s@husky.neu.edu>
2018-04-02 16:35:10 -0400

    Routing

--- ng5/src/app/about/about.component.ts
+++ ng5/src/app/about/about.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
-
+import { Router } from "@angular/router"; 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -8,11 +8,15 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.params.subscribe(res => console.log(res.id))
    }
 
   ngOnInit() {
+  }
+
+  sendMeHome(){
+    this.router.navigate(['']);
   }
 
 }
