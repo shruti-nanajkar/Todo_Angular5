ab53b856526d143ba4362ea865966398a10e371d
Shruti Nanajkar <nanajkar.s@husky.neu.edu>
2018-04-02 12:32:10 -0400

    routing and modified scss

--- ng5/src/app/app-routing.module.ts
+++ ng5/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";//added this
+import { AboutComponent } from "./about/about.component";//added this
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about/:id',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
