3da64f89891250b642721a5fa7849ff5876e409c
Shruti Nanajkar <nanajkar.s@husky.neu.edu>
2018-04-03 14:59:08 -0400

    Services

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -36,21 +37,25 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add task';
   goalText: string = 'Grocery';
-  goals = ['Laundary','Study','Gym'];
-  constructor() { }
+  goals = [];
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
     this.itemCount = this.goals.length;
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
   }
 
   addItem(){
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i){
     this.goals.splice(i,1);
     this.itemCount = this.goals.length - 0;
+    this._data.changeGoal(this.goals);
   }
 }
